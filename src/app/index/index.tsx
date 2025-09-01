fbb7b9de485e62646f67dcf12172341e9eff71dd
Evan Silva <evanildosilva467@gmail.com>
2025-09-01 20:31:49 -0300

    Utilizando Propriedades no Componente

--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -16,7 +16,9 @@ export default function Index() {
         </TouchableOpacity>
       </View>
 
-      <Category />
+      <Category name= "Projeto" icon="code"/>
+      <Category name= "Site" icon="language"/>
+      <Category name= "Video" icon="movie"/>
     </View>
   );
 }
