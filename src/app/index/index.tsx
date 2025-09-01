e7ee5c369f33e00e0c2251c6cdf5fa599c7acd4f
Evan Silva <evanildosilva467@gmail.com>
2025-09-01 15:59:27 -0300

    Criando o componente Category

--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -2,6 +2,8 @@ import { Image, View, TouchableOpacity } from "react-native";
 import { MaterialIcons } from "@expo/vector-icons";
 import { colors } from "@/styles/colors";
 
+import { Category } from "@/components/category";
+
 import { styles } from "./styles";
 
 export default function Index() {
@@ -9,10 +11,12 @@ export default function Index() {
     <View style={styles.container}>
       <View style={styles.header}>
         <Image source={require("@/assets/logo.png")} style={styles.logo} />
-        <TouchableOpacity>
+        <TouchableOpacity activeOpacity={0.7}>
           <MaterialIcons name="add" size={32} color={colors.green[300]} />
         </TouchableOpacity>
       </View>
+
+      <Category />
     </View>
   );
 }
