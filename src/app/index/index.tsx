e28bc99f61b5d04ee0b966b6cfe126cf6e4a67a3
Evan Silva <evanildosilva467@gmail.com>
2025-09-03 16:26:28 -0300

    Utilizando Modal

--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -1,4 +1,4 @@
-import { Image, View, TouchableOpacity, FlatList } from "react-native";
+import { Image, View, TouchableOpacity, FlatList, Modal, Text } from "react-native";
 import { MaterialIcons } from "@expo/vector-icons";
 import { colors } from "@/styles/colors";
 
@@ -17,21 +17,37 @@ export default function Index() {
         </TouchableOpacity>
       </View>
       <Categories />
-
-
       <FlatList
         data={["1", "2", "3"]}
         keyExtractor={(item) => item}
         renderItem={() => (<Link
           name="Rocketseat"
           url="https://www.rocketseat.com.br/"
-          onDetails={() => console.log("Clicou!")}
-        />
+          onDetails={() => console.log("Clicou!")} />
         )}
         style={styles.links}
         contentContainerStyle={styles.linksContente}
         showsHorizontalScrollIndicator={false}
       />
+      <Modal transparent visible={true}>
+        <View style={styles.modal}>
+          <View style={styles.modalContent}>
+            <View style={styles.modalHeader}>
+              <Text style={styles.modalCategory}>Curso</Text>
+              <TouchableOpacity activeOpacity={0.7}>
+                <MaterialIcons
+                  name="close"
+                  size={20}
+                  color={colors.gray[400]} />
+              </TouchableOpacity>
+            </View>
+            <View>
+              <Text style={styles.modalLinkName}>Rocketseat</Text>
+              <Text style={styles.modalUrl}>https://www.rocketseat.com.br/</Text>
+            </View>
+          </View>
+        </View>
+      </Modal>
     </View>
   );
 }
