ba5641a3a91857f3db6b64808f39b386b5a19060
Evan Silva <evanildosilva467@gmail.com>
2025-09-05 10:47:51 -0300

    Criando a tela para Adicionar novos links

---
+++ src/app/add/styles.ts
@@ -0,0 +1,25 @@
+import { StyleSheet } from 'react-native';
+import { colors } from '@/styles/colors';
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,  
+    paddingTop: 64, 
+  },
+  header: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingHorizontal: 24,
+    marginBottom: 24,
+  },
+  title:{
+    fontSize: 24,
+    fontWeight: '600',
+    color: colors.gray[200],
+  },
+  label:{
+    fontSize: 14,
+    color: colors.gray[400],    
+    paddingHorizontal: 24,
+  }
+});    
