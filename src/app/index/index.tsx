1cfbb9f6fe4ce4bab958de687b8caff36cc047a3
Evan Silva <evanildosilva467@gmail.com>
2025-09-09 11:08:53 -0300

    Utilizando o useFocusEffect

--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -1,7 +1,7 @@
-import { useState, useEffect } from "react";
+import { useState, useCallback } from "react";
 import { Image, View, TouchableOpacity, FlatList, Modal, Text, Alert } from "react-native";
 import { MaterialIcons } from "@expo/vector-icons";
-import { router } from "expo-router";
+import { router, useFocusEffect } from "expo-router";
 
 import { colors } from "@/styles/colors";
 import { styles } from "./styles";
@@ -26,9 +26,10 @@ export default function Index() {
     }
   }
 
-  useEffect(() => {
-    getLinks();
-  }, [category]);
+  useFocusEffect(
+    useCallback(() => {
+      getLinks();
+    }, []));
 
   return (
     <View style={styles.container}>
